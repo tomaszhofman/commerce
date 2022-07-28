f7057ddd821d5a1cc167481b3c5a9484e512362a
Tomasz Hofman <tomasz.k.hofman@gmail.com>
2022-07-28 22:58:54 +0200

    feat: introduce Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,56 @@
+import { SearchIcon, ShoppingCartIcon, UserIcon } from '@heroicons/react/outline';
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className="relative z-10">
+      <nav aria-label="Top">
+        <div className="bg-white">
+          <div className="border-b border-gray-200">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="h-16 flex items-center justify-between">
+                <div className="hidden h-full lg:flex">{/* Navigation */}</div>
+
+                <div className="flex-1 flex items-center justify-end">
+                  <div className="flex items-center lg:ml-8">
+                    <div className="flex space-x-8">
+                      <div className="hidden lg:flex">
+                        <a href="#" className="-m-2 p-2 text-gray-400 hover:text-gray-500">
+                          <span className="sr-only">Search</span>
+                          <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                        </a>
+                      </div>
+
+                      <div className="flex">
+                        <a href="#" className="-m-2 p-2 text-gray-400 hover:text-gray-500">
+                          <span className="sr-only">Account</span>
+                          <UserIcon className="w-6 h-6" aria-hidden="true" />
+                        </a>
+                      </div>
+                    </div>
+
+                    <span className="mx-4 h-6 w-px bg-gray-200 lg:mx-6" aria-hidden="true" />
+
+                    <div className="flow-root">
+                      <a href="#" className="group -m-2 p-2 flex items-center">
+                        <ShoppingCartIcon
+                          className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                          aria-hidden="true"
+                        />
+                        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                          0
+                        </span>
+                        <span className="sr-only">items in cart, view bag</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export { Header };
